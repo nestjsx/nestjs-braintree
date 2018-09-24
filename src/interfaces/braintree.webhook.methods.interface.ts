ec6c33e7de31d2597af8047c1bac33966f1ffa83
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-09-24 12:22:05 +0100

    Resolved issue with Interface

--- src/interfaces/braintree.webhook.methods.interface.ts
+++ src/interfaces/braintree.webhook.methods.interface.ts
@@ -1,8 +1,8 @@
-export interface BraintreeWebhookMethodTreeInterface {
-  [k: string]: BraintreeMethodInterface[];
-}
-
 export interface BraintreeWebhookMethodInterface {
   provider: string;
   method: string;
+}
+
+export interface BraintreeWebhookMethodTreeInterface {
+  [k: string]: BraintreeWebhookMethodInterface[];
 }
