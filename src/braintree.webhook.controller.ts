9281d05b2ed9babf84209d1507e4dcbec78ed490
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-09-26 14:07:57 +0100

    Removed redunant comments'

--- src/braintree.webhook.controller.ts
+++ src/braintree.webhook.controller.ts
@@ -11,11 +11,7 @@ export default class BraintreeWebhookController {
 
   @Post('webhook')
   async handle(@Req() request) {
-    //TODO parse the payload from braintree
-		//TODO get the type of webhook it is 
-		//TODO call relative function for that type of webhook
-    
-    let webhook: BraintreeWebhookNotificationInterface; //TODO add webhook type
+    let webhook: BraintreeWebhookNotificationInterface;
 
 		try {
        webhook = await this.braintree.parseWebhook({
@@ -28,9 +24,5 @@ export default class BraintreeWebhookController {
       Logger.warn('Failed to process webhook', this.constructor.name);
       throw new HttpException('Failed', 500);
     }
-
-    // //TODO call webhookProvider 
-    // //TODO again see braintree to see how to handle errors
-    
   }
 }
