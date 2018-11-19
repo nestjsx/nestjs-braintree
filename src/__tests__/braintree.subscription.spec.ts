2044740125a3ce52fd75df5136b5a5fccad4e3cc
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-11-19 22:58:12 +0000

    Added subscription tests file  - Requires paymentMethodToken to be implemented

---
+++ src/__tests__/braintree.subscription.spec.ts
@@ -0,0 +1,35 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { ConfigModule, ConfigService } from 'nestjs-config';
+import * as path from 'path';
+import { BraintreeModule, BraintreeProvider } from './../../src';
+
+describe('Braintree subscription methods', () => {
+  let module: TestingModule;
+  let provider: BraintreeProvider;
+
+  beforeEach(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        ConfigModule.load(
+          path.resolve(__dirname, '__stubs__', 'config', '*.ts'),
+        ),
+        BraintreeModule.forRootAsync({
+          useFactory: async config => config.get('braintree'),
+          inject: [ConfigService],
+        }),
+      ],
+    }).compile();
+
+    provider = module.get(BraintreeProvider);
+  });
+
+  // it('Create Subscription', async () => {
+
+  //   //TODO implement paymentMethodToken somehow. Try this https://developers.braintreepayments.com/reference/request/payment-method/create/node
+
+  //   // const result = await provider.createSubscription({
+  //   //   paymentMethodToken: '',
+  //   //   planId: 'c8vr',
+  //   // });
+  // });
+});
